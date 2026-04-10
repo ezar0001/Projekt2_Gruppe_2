f52e6af2acca4294ab4aca5fecfa09f395545810
ezar0001 <ezraarulampalam@MacBook-Pro-tilhrende-Ezra.local>
2026-04-10 10:40:09 +0200

    init commit

---
+++ src/main/java/org/projekt2_gruppe_2/Projekt2Gruppe2Application.java
@@ -0,0 +1,13 @@
+package org.projekt2_gruppe_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projekt2Gruppe2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Projekt2Gruppe2Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/projekt2_gruppe_2/Projekt2Gruppe2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.projekt2_gruppe_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projekt2Gruppe2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
